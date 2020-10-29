291060a00842f6e73843dcd161816c1407db5398
Jose Lopez <lezep2@gmail.com>
2020-10-29 15:57:00 -0700

    added file

---
+++ fixed_XOR/fixed_XOR.py
@@ -0,0 +1,9 @@
+input1 = "1c0111001f010100061a024b53535009181c"
+input2= "686974207468652062756c6c277320657965"
+
+message1=   input1.upper()
+message2=   input2.upper()  
+
+xor_output=input1^input2
+
+print(xor_output)
