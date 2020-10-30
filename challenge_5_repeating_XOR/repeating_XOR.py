f88e4a26b6813a3b5ef616cb6403eccd5e54e9c1
Jose Lopez <lezep2@gmail.com>
2020-10-30 09:37:07 -0700

    added separate method for multi-byte XOR

--- challenge_5_repeating_XOR/repeating_XOR.py
+++ challenge_5_repeating_XOR/repeating_XOR.py
@@ -1,9 +1,19 @@
 
+#I C E  I C E   I C E 
+
+
+def single_byte_XOR(input_bytes,char_value):
+    output_bytes = b''
+    for byte in input_bytes:
+        output_bytes+=bytes([byte^char_value])
+
+    return output_bytes
+
 def main():
     input = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
     key = "ICE"
     count = 0
-    output = ""
+    output = ''
     for i in input:
         #print (i)
         xor = i^key[count]
