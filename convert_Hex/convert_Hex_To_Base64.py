e68e9b7be4e4c34eef2d32763e547d1aff276663
Jose Lopez <lezep2@gmail.com>
2020-10-29 18:41:49 -0700

    fixed output for xor

--- convert_Hex/convert_Hex_To_Base64.py
+++ convert_Hex/convert_Hex_To_Base64.py
@@ -6,6 +6,7 @@ message_input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706
 
 message = message_input.upper()
 message_bytes = message.encode('ascii') #Now I have a raw bitstring
+print(message_bytes)
 base64_bytes = base64.b16decode(message_bytes)
 base64_message=base64.b64encode(base64_bytes)
 
--- fixed_XOR/fixed_XOR.py
+++ fixed_XOR/fixed_XOR.py
@@ -1,9 +1,16 @@
+import base64
+
 input1 = "1c0111001f010100061a024b53535009181c"
 input2= "686974207468652062756c6c277320657965"
 
-message1=   input1.upper()
-message2=   input2.upper()  
+#message1=   input1.upper()
+#message2=   input2.upper()  
 
-xor_output=input1^input2
+dec1 = int(input1,16)
+dec2=int(input2,16)
+xor=dec1^dec2
 
-print(xor_output)
+#xor = input1_bytes^input2_bytes
+
+
+print(xor)
