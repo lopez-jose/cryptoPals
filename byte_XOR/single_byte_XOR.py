8248e36f9d402030153dd0e644660cda3bd71034
Jose Lopez <lezep2@gmail.com>
2020-10-29 23:05:35 -0700

    changed the score method

--- byte_XOR/single_byte_XOR.py
+++ byte_XOR/single_byte_XOR.py
@@ -9,21 +9,35 @@ def single_byte_XOR(input_bytes,char_value):
 
 #frequency in english of letters. E is the most common count # of occurences, then divide by length, find the difference, add difference
 #smallest difference is equal to the score. 
+#input the actual bytes
+def score(input_bytes,char_value):
 
-#def score():
+    array = [12,9,8,7,7,7,6,6]
+    output_bytes=single_byte_XOR(input_bytes,char_value)
+    sum_e = 0
 
-#array = [12,9,8,7,7,7,6,6]
-    
+    for i in range(0,len(output_bytes)):
+        if(output_bytes[i]<100):
+            print(output_bytes[i])
+
+        
+    #print(output_bytes)
+    return 3
 
 
 def main():
 
     input_1 = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    input_bytes = bytes.fromhex(input_1)
-   
+    input_bytes= bytes.fromhex(input_1)
+    get_score = 0
+    smallest_score = 0
 
     for i in range(256):
-        print(single_byte_XOR(input_bytes,i))
+        get_score=score(input_bytes,i)
+        
+
+        
+        
 
     
 
