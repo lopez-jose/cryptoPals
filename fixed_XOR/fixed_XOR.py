1c3801a49e58c242974fd20e679ceda52b82d740
Jose Lopez <lezep2@gmail.com>
2020-10-29 18:48:42 -0700

    changed xor method

--- fixed_XOR/fixed_XOR.py
+++ fixed_XOR/fixed_XOR.py
@@ -1,16 +1,25 @@
 import base64
 
-input1 = "1c0111001f010100061a024b53535009181c"
-input2= "686974207468652062756c6c277320657965"
-
-#message1=   input1.upper()
-#message2=   input2.upper()  
-
-dec1 = int(input1,16)
-dec2=int(input2,16)
-xor=dec1^dec2
-
-#xor = input1_bytes^input2_bytes
 
 
-print(xor)
+def hex_xor(input_1, input_2):
+    bits_1=
+
+def main():
+    input1 = "1c0111001f010100061a024b53535009181c"
+    input2= "686974207468652062756c6c277320657965"
+
+    xor=hex_xor(input1,input2)
+
+
+
+    dec1 = int(input1,16)
+    dec2=int(input2,16)
+    xor=dec1^dec2
+
+    #xor = input1_bytes^input2_bytes
+
+
+    print(xor)
+
+main()
