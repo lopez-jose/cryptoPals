b2ceb71ecf53e294d3835118691ac3962a3c23ba
Jose Lopez <lezep2@gmail.com>
2020-10-30 22:23:50 -0700

    added hamming distanced method

--- ch_6_break_repeating_key_XOR/break_key_XOR.py
+++ ch_6_break_repeating_key_XOR/break_key_XOR.py
@@ -1,19 +1,10 @@
 import base64
 
-
-
-def main():
-    input_1 = "this is a test"
-    input_2 = "wokka wokka!!!"
-
-
+def hamming_distance(bytes_1,bytes_2):
     index = 0
-
-    input_1_bytes = bytes(input_1,'utf-8')
-    input_2_bytes = bytes(input_2,'utf-8')
     XOR_bytes = b''
-    for byte in input_1_bytes:
-        x = bytes([byte^input_2_bytes[index]])
+    for byte in bytes_1:
+        x = bytes([byte^bytes_2[index]])
         XOR_bytes+=x
         index+=1
 
@@ -21,14 +12,26 @@ def main():
 
     count = 0
     for byte in XOR_bytes:
-        print(byte)
         temp = byte
         for i in range(7):
             if(temp&1!=0):
                 count+=1
             temp = temp>>1
-            print(temp)  
-    print(count)
+            
+    return count
+
+def main():
+    input_1 = "this is a test"
+    input_2 = "wokka wokka!!!"
+
+
+    
+
+    bytes_1 = bytes(input_1,'utf-8')
+    bytes_2 = bytes(input_2,'utf-8')
+
+    print(hamming_distance(bytes_1,bytes_2))
+    
 
 
 
