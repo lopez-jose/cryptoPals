ba8951730d1fead1d3a26417807fee65c6453d18
Jose Lopez <lezep2@gmail.com>
2020-10-30 20:49:25 -0700

    added key_XOR method definition

--- challenge_5_repeating_XOR/repeating_XOR.py
+++ challenge_5_repeating_XOR/repeating_XOR.py
@@ -1,34 +1,27 @@
-
-#I C E  I C E   I C E 
 import base64
 
-def single_byte_XOR(input_bytes,char_value):
-    output_bytes = b''
-    for byte in input_bytes:
-        output_bytes+=bytes([byte^char_value])
-
-    return output_bytes
-
+#receives raw bytes and returns the XOR of input_bytes with key_bytes
 def key_XOR(input_bytes,key_bytes):
-    return 0
-
-
-def main():
-    input = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
-    key = "ICE"
-    input_bytes=bytes(input,'utf-8')
-    key_bytes= bytes(key,'utf-8')
     index = 0;
     output_bytes = b''
     for byte in input_bytes:
         output_bytes+=bytes([byte^key_bytes[index]])
         index+=1
-        if(index ==3):
+        if(index == len(key_bytes)):
             index = 0
-    #for byte in key_bytes:
-       # print(byte,end=' ')
+
+    return output_bytes
+
+
+def main():
+    input_string = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal"
+    key = "ICE"
+    input_bytes=bytes(input_string,'utf-8')
+    key_bytes= bytes(key,'utf-8')
+
+    output_bytes = key_XOR(input_bytes,key_bytes)
+
     base16_bytes = base64.b16encode(output_bytes)
-    #xor = input_bytes[0]^key_bytes[0]
     print(base16_bytes)
    
 
