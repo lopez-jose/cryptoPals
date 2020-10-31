c5e9ff4d03b05dc5e6003bd70b3970f9b0f6e138
Jose Lopez <lezep2@gmail.com>
2020-10-30 22:26:39 -0700

    formatted code with pycodestyle

--- ch_6_break_repeating_key_XOR/break_key_XOR.py
+++ ch_6_break_repeating_key_XOR/break_key_XOR.py
@@ -1,38 +1,33 @@
 import base64
 
-def hamming_distance(bytes_1,bytes_2):
+
+def hamming_distance(bytes_1, bytes_2):
     index = 0
     XOR_bytes = b''
     for byte in bytes_1:
-        x = bytes([byte^bytes_2[index]])
-        XOR_bytes+=x
-        index+=1
-
-    
+        x = bytes([byte ^ bytes_2[index]])
+        XOR_bytes += x
+        index += 1
 
     count = 0
     for byte in XOR_bytes:
         temp = byte
         for i in range(7):
-            if(temp&1!=0):
-                count+=1
-            temp = temp>>1
-            
+            if(temp & 1 != 0):
+                count += 1
+            temp = temp >> 1
+
     return count
+
 
 def main():
     input_1 = "this is a test"
     input_2 = "wokka wokka!!!"
 
+    bytes_1 = bytes(input_1, 'utf-8')
+    bytes_2 = bytes(input_2, 'utf-8')
 
-    
-
-    bytes_1 = bytes(input_1,'utf-8')
-    bytes_2 = bytes(input_2,'utf-8')
-
-    print(hamming_distance(bytes_1,bytes_2))
-    
-
+    print(hamming_distance(bytes_1, bytes_2))
 
 
 main()
