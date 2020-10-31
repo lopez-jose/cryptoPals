b24aad6333947c76980db1c17917286135c37f95
Jose Lopez <lezep2@gmail.com>
2020-10-30 21:56:08 -0700

    implemented an ANDind section for bytes

--- ch_6_break_repeating_key_XOR/break_key_XOR.py
+++ ch_6_break_repeating_key_XOR/break_key_XOR.py
@@ -1,23 +1,38 @@
 import base64
 
+
+
 def main():
-    print("hello world")
     input_1 = "this is a test"
     input_2 = "wokka wokka!!!"
 
 
+    index = 0
 
     input_1_bytes = bytes(input_1,'utf-8')
     input_2_bytes = bytes(input_2,'utf-8')
-    matching_bits = b''
+    AND_bytes = b''
     for byte in input_1_bytes:
-        matching_bits+=bytes([input_1_bytes]^input_2_bytes[byte])
+        x = bytes([byte^input_2_bytes[index]])
+        AND_bytes+=bytes([byte^input_2_bytes[index]])
+        print(bytes([byte^input_2_bytes[index]]))
+        
+        index+=1
 
-    print(matching_bits)
-    for i in input_1_bytes:
-        print(i)
-    print(input_1_bytes)
+    
 
+    count = 0
+    for byte in AND_bytes:
+        print(byte)
+        temp = byte
+        #print(temp)
+        for i in range(7):
+            if(temp&1!=0):
+                count+=1
+            temp>>1
+            #print(temp)
+        #print(count)    
+    #print(count)
 
 
 
