7cd2d6bd9808cafadfd518689bfb7ee936539447
Jose Lopez <lezep2@gmail.com>
2020-10-30 22:16:18 -0700

    added correct hamming distance

--- ch_6_break_repeating_key_XOR/break_key_XOR.py
+++ ch_6_break_repeating_key_XOR/break_key_XOR.py
@@ -11,28 +11,24 @@ def main():
 
     input_1_bytes = bytes(input_1,'utf-8')
     input_2_bytes = bytes(input_2,'utf-8')
-    AND_bytes = b''
+    XOR_bytes = b''
     for byte in input_1_bytes:
         x = bytes([byte^input_2_bytes[index]])
-        AND_bytes+=bytes([byte^input_2_bytes[index]])
-        print(bytes([byte^input_2_bytes[index]]))
-        
+        XOR_bytes+=x
         index+=1
 
     
 
     count = 0
-    for byte in AND_bytes:
+    for byte in XOR_bytes:
         print(byte)
         temp = byte
-        #print(temp)
         for i in range(7):
             if(temp&1!=0):
                 count+=1
-            temp>>1
-            #print(temp)
-        #print(count)    
-    #print(count)
+            temp = temp>>1
+            print(temp)  
+    print(count)
 
 
 
