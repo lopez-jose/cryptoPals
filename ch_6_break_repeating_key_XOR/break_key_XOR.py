eb9741cd2f081abd1803296e80743c501f74ac0e
Jose Lopez <lezep2@gmail.com>
2020-11-29 21:45:43 -0800

    added comments to methods

--- ch_6_break_repeating_key_XOR/break_key_XOR.py
+++ ch_6_break_repeating_key_XOR/break_key_XOR.py
@@ -1,17 +1,21 @@
 import base64
 
+
+# character frequency
 CHARACTER_FREQ = {
     'a': 0.0651738, 'b': 0.0124248, 'c': 0.0217339, 'd': 0.0349835, 'e': 0.1041442, 'f': 0.0197881, 'g': 0.0158610,
     'h': 0.0492888, 'i': 0.0558094, 'j': 0.0009033, 'k': 0.0050529, 'l': 0.0331490, 'm': 0.0202124, 'n': 0.0564513,
     'o': 0.0596302, 'p': 0.0137645, 'q': 0.0008606, 'r': 0.0497563, 's': 0.0515760, 't': 0.0729357, 'u': 0.0225134,
     'v': 0.0082903, 'w': 0.0171272, 'x': 0.0013692, 'y': 0.0145984, 'z': 0.0007836, ' ': 0.1918182
 }
- #the hamming class is used to store an object where index and distance are recorded
+# the hamming class is used to store an object where index and distance are recorded
+
+
 class Hamming:
-    def __init__(self,index,distance):
-        self.index = index  
+    def __init__(self, index, distance):
+        self.index = index
         self.distance = distance
-   
+
 
 def single_byte_XOR(input_bytes, char_value):
     output_bytes = b''
@@ -32,17 +36,19 @@ def score(input_bytes, char_value):
     return sum
 
 # finds the hamming distance between two even sized byte strings
+
+
 def hamming_distance(bytes_1, bytes_2):
     index = 0
     XOR_bytes = b''
     difference_count = 0
 
-    #XORs bytes in bytes_1 and bytes_2
+    # XORs bytes in bytes_1 and bytes_2
     for byte in bytes_1:
         xor = bytes([byte ^ bytes_2[index]])
         XOR_bytes += xor
         index += 1
-    #takes bits in each byte of XOR_bytes and adds +1 to difference_count if = 1
+    # takes bits in each byte of XOR_bytes and adds +1 to difference_count if = 1
     for byte in XOR_bytes:
         temp = byte
         for i in range(7):
@@ -52,9 +58,11 @@ def hamming_distance(bytes_1, bytes_2):
 
     return difference_count
 
-def hamming_distance_list(string_bytes,list_length):
+
+def hamming_distance_list(string_bytes, list_length):
 
     return 0
+
 
 def main():
     # Opens file.txt and reads into line
@@ -63,12 +71,12 @@ def main():
 
     s = ""
     s += line
-    #loops f.readline and adds input to the s string
+    # loops f.readline and adds input to the s string
     while line:
         line = f.readline()
         s += line
 
-    #converts s into bytes and removes utf-8 encoding
+    # converts s into bytes and removes utf-8 encoding
     string_bytes = bytes(s, 'utf-8')
     strings = []
     decrypted_strings = []
@@ -111,21 +119,21 @@ def main():
             pos = 0
     print(decrypted)
 
-
     my_hamming = []
-    
+
     for i in range(39):
         i += 2
         length = len(string_bytes[:i])
         avg_hamming = hamming_distance(
             string_bytes[:i], string_bytes[i:2*i])/length
-        my_hamming.append(Hamming(i,avg_hamming))
-    new_hamming_list = sorted(my_hamming,key = lambda Hamming: Hamming.distance)
-    print("The sorted list will now be printed")    
-    for i in range (len(new_hamming_list)):
-        print("Length = ", end = " ")
-        print(new_hamming_list[i].index, end = " \t")
-        print (" Hamming Distance = ", end = " \t")
-        print(round((new_hamming_list[i].distance),3))
-        
+        my_hamming.append(Hamming(i, avg_hamming))
+    new_hamming_list = sorted(my_hamming, key=lambda Hamming: Hamming.distance)
+    print("The sorted list will now be printed")
+    for i in range(len(new_hamming_list)):
+        print("Length = ", end=" ")
+        print(new_hamming_list[i].index, end=" \t")
+        print(" Hamming Distance = ", end=" \t")
+        print(round((new_hamming_list[i].distance), 3))
+
+
 main()
